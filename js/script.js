5a4352ea6db987f058e80e4463a12344cdb3370a
Liliana Czeredys <liliana.czeredys@gmail.com>
2019-07-17 22:00:29 +0200

    add playerMove

---
+++ js/script.js
@@ -0,0 +1,8 @@
+printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let computerMove = `kamień`;
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to remis!');
