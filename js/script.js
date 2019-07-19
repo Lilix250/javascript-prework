6aa361f91846c5f7fe06e8b580d7e1d286a9bc7a
Liliana Czeredys <liliana.czeredys@gmail.com>
2019-07-19 21:37:09 +0200

    add buttons

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
+function playGame(playerInput) {
+    /*let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.'); */
+    
+    clearMessages();
+    
+    let playerMove = getMoveName(playerInput);
+
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+    let computerMove = getMoveName(randomNumber);
+
+    displayResult(computerMove, playerMove);
+}
 
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+document.getElementById('play-rock').addEventListener('click', function(){
+    printMessage('Guzik "Kamień" został kliknięty');
+    playGame(1);
+  });
 
-let playerMove = getMoveName(playerInput);
+  document.getElementById('play-paper').addEventListener('click', function(){
+    printMessage('Guzik "Papier" został kliknięty');
+    playGame(2);
+  });
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-let computerMove = getMoveName(randomNumber);
-
-displayResult(computerMove, playerMove);
+  document.getElementById('play-scissors').addEventListener('click', function(){
+    printMessage('Guzik "Nożyce" został kliknięty');
+    playGame(3);
+  });
